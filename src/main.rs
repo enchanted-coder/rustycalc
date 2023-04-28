e7f81b3667caf95a7b91194986bee5b8938a440a
enchanted-coder <danieltogey@pm.me>
2023-04-28 14:22:40 +0300

    interesting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 
 fn main() {
-    println!("hello world");
+    
+    use std::io;
+    print!("Guess the number: \n");
+    let mut num = String::new();
+
+    io::stdin().read_line(&mut num).expect("You didn't enter anything");
+
+    print!("You guessed the number: {}\n", num)
 }
