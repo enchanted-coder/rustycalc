35953322aed2637d50cfe00ad45a8936a1f37c68
enchanted-coder <danieltogey@pm.me>
2023-04-28 20:36:59 +0300

    tui calculator complete

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,92 @@
+use std::io;
 
 fn main() {
+    let mut choice: String = String::new();
+    println!("What operation do you want to apply to these numbers?\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
+    io::stdin().read_line(&mut choice).expect("msg");
+
     
-    use std::io;
-    print!("Guess the number: \n");
-    let mut num = String::new();
+    
 
-    io::stdin().read_line(&mut num).expect("You didn't enter anything");
+    let anotate: i32 = choice.trim().parse().expect("msg");
 
-    print!("You guessed the number: {}\n", num)
+    if anotate == 1 {
+        let mut a = String::new();
+        println!("Input first number: ");
+        io::stdin().read_line(&mut a).expect("msg");
+
+        let mut b = String::new();
+        println!("Input second number: ");
+        io::stdin().read_line(&mut b).expect("msg");
+
+
+        let ans = addition(a.trim().parse().expect("msg"), b.trim().parse().expect("msg"));
+
+        println!("Your answer is: {}", ans);
+    }else if anotate == 2 {
+        let mut a = String::new();
+        println!("Input first number: ");
+        io::stdin().read_line(&mut a).expect("msg");
+
+        let mut b = String::new();
+        println!("Input second number: ");
+        io::stdin().read_line(&mut b).expect("msg");
+
+
+        let ans = subtraction(a.trim().parse().expect("msg"), b.trim().parse().expect("msg"));
+
+        println!("Your answer is: {}", ans);
+
+    }else if anotate == 3 {
+        let mut a = String::new();
+        println!("Input first number: ");
+        io::stdin().read_line(&mut a).expect("msg");
+
+        let mut b = String::new();
+        println!("Input second number: ");
+        io::stdin().read_line(&mut b).expect("msg");
+
+
+        let ans = multiplication(a.trim().parse().expect("msg"), b.trim().parse().expect("msg"));
+
+        println!("Your answer is: {}", ans);
+
+    }else if anotate == 4 {
+        let mut a = String::new();
+        println!("Input first number: ");
+        io::stdin().read_line(&mut a).expect("msg");
+
+        let mut b = String::new();
+        println!("Input second number: ");
+        io::stdin().read_line(&mut b).expect("msg");
+
+
+        let ans = division(a.trim().parse().expect("msg"), b.trim().parse().expect("msg"));
+
+        println!("Your answer is: {}", ans);
+
+    } else {
+        println!("unknown error");
+    }
+
+    
 }
+
+fn addition(a: i32, b: i32) -> i32 {
+    a+b
+}
+
+fn subtraction(a: i32, b: i32) -> i32 {
+    a-b
+}
+
+fn multiplication(a: i32, b: i32) -> i32 {
+    a*b
+}
+
+fn division(a: i32, b: i32) -> i32 {
+    a/b
+}
+
+
+
